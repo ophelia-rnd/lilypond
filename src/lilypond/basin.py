40ea6de6faa425579efd90b81d6ec8954c911c44
Matthew Balogh <meld-03-spouts@icloud.com>
2026-08-08 16:14:05 +0200

    Refactor base styling

--- src/lilypond/basin.py
+++ src/lilypond/basin.py
@@ -1,40 +1,8 @@
 from minisom import MiniSom
 from typing import Literal
-from lilypond.utils.colorscale import get_truncated_colorscale
+from lilypond.pond_base_style import PondBaseStyle
 
 class Basin:
-
-    STYLES = {
-        "pond": {
-            "water_colorscale": get_truncated_colorscale(colorscale="Blues", clip_interval=(.6, .9)),
-            "rhizome_colorscale": get_truncated_colorscale(colorscale="Greys", clip_interval=(.9, 1.0)),
-            "pad_colorscale": get_truncated_colorscale(colorscale="speed_r", clip_interval=(.0, .9)),
-            "petal_colorscale": get_truncated_colorscale(colorscale="Burg", clip_interval=(.0, .5)),
-            "petal_marker": lambda activation_strength, colorscale, sizes, marker_line: dict(
-                color=activation_strength, colorscale=colorscale, size=sizes, symbol="asterisk", opacity=1,
-                line=marker_line
-            ),
-            "petal_marker_line": lambda activation_strength, colorscale: dict(
-                width=2, color=activation_strength, colorscale=colorscale
-            ),
-            "petal_halo_marker": lambda sizes: dict(
-                color="black", size=sizes * 1.2, symbol="asterisk", opacity=.6,
-                line=dict(width=6, color="black")
-            ),
-        },
-        "iceflock": {
-            "water_colorscale": get_truncated_colorscale(colorscale="Blues", clip_interval=(.6, .9)),
-            "rhizome_colorscale": get_truncated_colorscale(colorscale="Greys", clip_interval=(.9, 1.0)),
-            "pad_colorscale": get_truncated_colorscale(colorscale="gray_r", clip_interval=(.0, .1)),
-            "petal_colorscale": get_truncated_colorscale(colorscale="gray_r", clip_interval=(.8, 1.0)),
-            "petal_marker": lambda activation_strength, colorscale, sizes, marker_line: dict(
-                color=activation_strength, colorscale=colorscale, size=sizes, symbol="triangle-se", opacity=1,
-                line=marker_line
-            ),
-            "petal_marker_line": lambda activation_strength, colorscale: dict(width=0),
-            "petal_halo_marker": lambda sizes: dict(),
-        }
-    }
 
     def __init__(self, som: MiniSom, random_seed=None, verbose=False):
         self.som = som
@@ -99,10 +67,10 @@ class Basin:
         from lilypond.legacy_pond import LegacyPond
         return LegacyPond(self, verbose=self.verbose)
 
-    def pond(self, style_name:Literal["pond", "iceflock"]="pond"):
+    def pond(self, base_style: Literal["pond", "iceflock"] | PondBaseStyle= "pond"):
         self.__assert_prepared()
         from lilypond.pond import Pond
-        return Pond(self, style_name=style_name, verbose=self.verbose)
+        return Pond(self, base_style=base_style, verbose=self.verbose)
 
     def __assert_prepared(self):
         assert hasattr(self, "prepared_") and self.prepared_ is True, "The Basin must be prepared."
